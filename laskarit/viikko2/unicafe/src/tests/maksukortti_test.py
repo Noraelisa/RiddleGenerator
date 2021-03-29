bbf485ac27d48a461a7168e91630a157dfa6362f
Nora Elisa <nora.heikkila@helsinki.fi>
2021-03-29 21:23:11 +0300

    maksukortin testit+gitignoren paivitys

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,24 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+
+    def test_kortille_voi_ladata_rahaa(self):
+        self.maksukortti.lataa_rahaa(10)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.2")   
+
+    def test_ota_rahaa_vahentaa_saldoa_oikein(self):
+        self.maksukortti.ota_rahaa(5)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.05")
+
+    def test_ota_rahaa_ei_vie_saldoa_negatiiviseksi(self):
+        self.maksukortti.ota_rahaa(16)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1") 
+
+    def test_ota_rahaa_ilmoittaa_riittavatko_rahat(self):
+        if self.maksukortti.ota_rahaa(self.maksukortti.saldo < 10):
+            return False
+        elif self.maksukortti.ota_rahaa(self.maksukortti.saldo > 10):
+            return True
